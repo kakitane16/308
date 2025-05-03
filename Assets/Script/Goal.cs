506ef8758dc42d6f746fae57d6b3ed6d6e60a44f
WNHS30029\kakit <kaito.suzuk.2004@gmail.com>
2025-05-03 22:26:00 +0900

    Goalの判定を強化　鈴木海飛 今日の気持ち曇り　眠たいねみんな頑張ろ

--- Assets/Script/Goal.cs
+++ Assets/Script/Goal.cs
@@ -1,21 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
 
 public class Goal : MonoBehaviour
 {
-    private Transform player;
-    private Transform goal;
+    public Transform goal;
+    public float maxScore = 100f;
+    private int score;
+    private float Level;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        Level = 20f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision gl)
     {
-        
+        if (gl.gameObject.CompareTag("syari"))
+        {
+            //シャリとネタの中心点の距離を捕る
+            float distance = Vector3.Distance(transform.position, goal.position);
+            //スコアが0以上100以下に設定
+            //値が大きいのでレベルの値を大きくすると引かれる値が大きくなる
+            score = (int)Mathf.Clamp(maxScore - (distance * Level), 0f, maxScore);
+
+            Debug.Log("ゴールに触れた！ スコア: " + score);
+        }
+
+        //判定基準　bad <= 20  Nomal <= 40 Good <= 60  Great <= 80   81 <= Perfect <= 100 
+        if(score <= 20)
+        {
+            Debug.Log("Bad");
+        }
+        else if(score <= 40)
+        {
+            Debug.Log("Nomal");
+        }
+        else if(score <= 60)
+        {
+            Debug.Log("Good");
+        }
+        else if(score <= 80)
+        {
+            Debug.Log("Great");
+        }
+        else
+        {
+            Debug.Log("Perfect");
+        }
     }
 }
