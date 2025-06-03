4b1b845334e27950c454c6e14bc91920ed547bcf
Rio <rioriochannel@gmail.com>
2025-06-03 12:10:01 +0900

    ButtonSize.csにコメント追加

--- Assets/Script/Title/ButtonSize.cs
+++ Assets/Script/Title/ButtonSize.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// 選択されたボタンを大きさを変えるスクリプト。
+/// </summary>
 public class ButtonSize : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
-    private Vector3 _originalScale;
-    [SerializeField] private Vector3 _selectedScale = new Vector3(1.2f, 1.2f, 1.2f);
-    [SerializeField] private float _scaleSpeed = 10f;
+    private Vector3 _originalScale; //元のサイズ
+    [SerializeField] private Vector3 _selectedScale = new Vector3(1.2f, 1.2f, 1.2f); // 選択後のサイズ(インスペクターでサイズを変えれるように)
+    [SerializeField] private float _scaleSpeed = 10f; // 大きさが変わる速度(滑らかに変わるように)
 
     // ここにシーン名を入れる
-    [SerializeField] private string sceneToLoad;
+    //[SerializeField] private string sceneToLoad;
 
-    private Vector3 _targetScale;
-    private bool _isSelected = false;
+    private Vector3 _targetScale; // 選択対象
+    private bool _isSelected = false; //選択中かどうか
 
     public bool IsSelected => _isSelected;  // 外部から選択状態を取得可能に
 
@@ -21,25 +24,26 @@ public class ButtonSize : MonoBehaviour, ISelectHandler, IDeselectHandler
         _targetScale = _originalScale;
     }
 
+    // =======選択されたら徐々に大きさを変える========
     void Update()
     {
         transform.localScale = Vector3.Lerp(transform.localScale, _targetScale, Time.deltaTime * _scaleSpeed);
     }
-
+    // ==========選択中==========
     public void OnSelect(BaseEventData eventData)
     {
         _isSelected = true;
         _targetScale = _selectedScale;
     }
-
+    // ==========選択外==========
     public void OnDeselect(BaseEventData eventData)
     {
         _isSelected = false;
         _targetScale = _originalScale;
     }
 
-    public string GetSceneName()
-    {
-        return sceneToLoad;
-    }
+    //public string GetSceneName()
+    //{
+    //    return sceneToLoad;
+    //}
 }
