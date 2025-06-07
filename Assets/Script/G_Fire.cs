28941be6ef6d0016b38144f302b8a4294b3c95ce
Iorrrrri <mnmia307@gmail.com>
2025-06-07 21:47:07 +0900

    Buner用フォルダ追加　G_FireスクリプトにG_Weight変数追加　この値をいじるとネタの重量が変わる

--- Assets/Script/G_Fire.cs
+++ Assets/Script/G_Fire.cs
@@ -7,6 +7,7 @@ public class G_Fire : MonoBehaviour
     //例焼けた見た目の素材(マテリアル)をInspectorで設定できるように公開してる物
     //unityエディター上で寿司が焼けたりする時に表示させたいマテリアルをここにドラック＆ドロップするだけ
     public Color G_Color = new Color(0.0f, 0.0f, 0.0f);       // 焼き色マテリアル
+    public float G_Weight = 0.7f;
 
     //何かがバーナーにぶつかって来た時に自動で呼ばれる関数
     //otherはぶつかってきた相手例えば寿司とかを表している
@@ -18,6 +19,11 @@ public class G_Fire : MonoBehaviour
             //これはバーナーに触れた瞬間(other)上にあるbutnedMaterial(焼けた素材)から
             //Rendererを取り出してrendererに変更するもの
             Renderer renderer = other.GetComponent<Renderer>();
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+
+            // 重量の変更
+            if (rb != null)
+                rb.mass = G_Weight;
             //見た目を変える前に、必要なものがちゃんとあるか確認している項目
             //&&は両方がちゃんとtrue(ある)のときだけ下の処理を実行するという意味
             if (renderer != null && G_Color != null )
