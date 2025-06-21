919b1cbfdebd2aec4448b77f8bfb06f96cca8fe6
Iorrrrri <mnmia307@gmail.com>
2025-06-21 21:30:00 +0900

    playercameraの挙動を追加

--- Assets/Script/Player/PlayerCamera.cs
+++ Assets/Script/Player/PlayerCamera.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class PlayerCamera : MonoBehaviour
 {
     // グローバル変数
-    public Transform g_Target;      // 追従対象
+    private Transform g_Target;      // 追従対象
     public Transform g_FocusObject; // 注視対象
     public string g_TargetTag = "Player"; // 追従対象のタグ
     public Vector3 g_Offset = new Vector3(0.0f, -5.0f, -10.0f);  // カメラ相対位置
@@ -27,74 +27,74 @@ public class PlayerCamera : MonoBehaviour
     {
         this.transform.position = g_Position;
         this.transform.rotation = g_Rotation;
-
-        // 既に追従対象が設定されている場合は、位置を記録(現状の開発環境での動作確認用の仮置きです、プレハブ生成版に開発が切り替わった段階で削除してください)
-        if (g_Target != null) lasttargetpos = g_Target.position; // 追従対象の位置を記録
+        // Playerタグがついているオブジェクトを追従
+        GameObject playercamera = GameObject.FindGameObjectWithTag(g_TargetTag);
+        if (playercamera != null)
+        {
+            g_Target = playercamera.transform;
+            lasttargetpos = g_Target.position;  // 追従対象の位置を記録
+        }
     }
     // 全ての処理がおわった後に処理を行いたいので
     void LateUpdate()
     {
-        if (g_Target == null)   // 追従対象がnullの場合
-        {
-            // 追従対象を取得
-            GameObject player = GameObject.FindGameObjectWithTag(g_TargetTag);
-            if (player != null)
-            {
-                g_Target = player.transform;
-                lasttargetpos = g_Target.position;
-            }
-        }
-        else
-        {
             // 注視対象がいなかったら処理をしない(追従対象による条件は削除しました、既に判定したのちにこの処理が行われるため)
-            if (g_FocusObject == null) { return; }
-            // targetの移動量を取得
-            Vector3 movement = g_Target.position - lasttargetpos;
+      // if (g_FocusObject == null) { return; }
+       // targetの移動量を取得
+       Vector3 movement = g_Target.position - lasttargetpos;
+    
+       switch (state)
+       {
+           case n_CameraState.Idle:
+    
+               // Transform Camera = this.transform;
+               // Camera.position = g_Target.TransformPoint(g_Offset); // ネタの後方へ
+               //// Camera.LookAt(g_Target);
+               // Camera.rotation = g_Target.rotation;
+               //     /*Quaternion.Lerp(
+               //     transform.rotation, g_Target.rotation, Time.deltaTime * g_CameraSpeed);*/
+    
+               if (movement.magnitude > g_MovementThreshold) //動いたら
+               {
+                   state = n_CameraState.Focusing; // 状態遷移
+                   focusTimer = g_FocusDuration;   // 注視時間代入
+               }
+               break;
+    
+           case n_CameraState.Focusing:
+              // transform.LookAt(g_FocusObject); // 注視
+               focusTimer -= Time.deltaTime;    // 時間経過で遷移
+               if (focusTimer <= 0.0f)
+               {
+                   state = n_CameraState.Following;
+               }
+               break;
+    
+           case n_CameraState.Following:
+               Vector3 desiredPosition = g_Target.position + g_Offset;
+               transform.position = Vector3.Lerp(transform.position,
+                                    desiredPosition, Time.deltaTime * g_FollowSpeed); //滑らかに
 
-            switch (state)
-            {
-                case n_CameraState.Idle:
-
-                    // Transform Camera = this.transform;
-                    // Camera.position = g_Target.TransformPoint(g_Offset); // ネタの後方へ
-                    //// Camera.LookAt(g_Target);
-                    // Camera.rotation = g_Target.rotation;
-                    //     /*Quaternion.Lerp(
-                    //     transform.rotation, g_Target.rotation, Time.deltaTime * g_CameraSpeed);*/
-
-                    if (movement.magnitude > g_MovementThreshold) //動いたら
-                    {
-                        state = n_CameraState.Focusing; // 状態遷移
-                        focusTimer = g_FocusDuration;   // 注視時間代入
-                    }
-                    break;
-
-                case n_CameraState.Focusing:
-                    transform.LookAt(g_FocusObject); // 注視
-                    focusTimer -= Time.deltaTime;    // 時間経過で遷移
-                    if (focusTimer <= 0.0f)
-                    {
-                        state = n_CameraState.Following;
-                    }
-                    break;
-
-                case n_CameraState.Following:
-                    Vector3 desiredPosition = g_Target.position + g_Offset;
-                    transform.position = Vector3.Lerp(transform.position,
-                                         desiredPosition, Time.deltaTime * g_FollowSpeed); //滑らかに
-                    transform.LookAt(g_Target);
-                    if (movement.magnitude < g_MovementThreshold) //止まったら
-                    {
-                        // 3秒待つ
-                        g_Timer += Time.deltaTime;
-                        if (g_Timer >= g_WaitTime)
-                        {
-                            SceneManager.LoadScene(2);
-                        }
-                    }
-                    break;
-            }
-            lasttargetpos = g_Target.position;
-        }
+                // タグ "Table" がついているすべての GameObject を取得
+                GameObject[] tables = GameObject.FindGameObjectsWithTag("Table");
+                foreach (GameObject table in tables)
+                {
+                    // MeshRendererを取得し非表示にする
+                    MeshRenderer mr = table.GetComponent<MeshRenderer>();
+                    mr.enabled = false;
+                }
+                transform.LookAt(g_Target);
+               if (movement.magnitude < g_MovementThreshold) //止まったら
+               {
+                   // 3秒待つ
+                   g_Timer += Time.deltaTime;
+                   if (g_Timer >= g_WaitTime)
+                   {
+                       SceneManager.LoadScene(2);
+                   }
+               }
+               break;
+       }
+       lasttargetpos = g_Target.position;
     }
 }
