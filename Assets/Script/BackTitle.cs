ff6a1831adf61f42810124839646f75fd8b94a93
WNHS30029\kakit <kaito.suzuk.2004@gmail.com>
2025-07-04 17:04:19 +0900

    鈴木海飛

--- Assets/Script/BackTitle.cs
+++ Assets/Script/BackTitle.cs
@@ -7,16 +7,55 @@ using UnityEngine.InputSystem;
 public class BackTitle : MonoBehaviour
 {
     GamePadCommand command;
+    int change;
     public void Start()
     {
         command = new GamePadCommand();
+        change = 0;//現在の位置　0が上　１が下
     }
+
     public void Update()
     {
+        //ゲームパッドやキーボードで同じ処理ができるようなプログラムを取得
         command = new GamePadCommand();
+
+        //ESCキーが押されるまでは以下の処理は入らない
         if (command.GetEscKey((int)GameManager.Instance.inputDevice))
         {
-            SceneManager.LoadScene("Title");
+            //上にUI移動
+            if (command.UpAction((int)GameManager.Instance.inputDevice))
+            {
+                if (change == 1)
+                {
+                    change = 0;
+                }
+            }
+            //下に移動
+            if (command.DownAction((int)GameManager.Instance.inputDevice))
+            {
+                if (change == 0)
+                {
+                    change = 1;
+                }
+            }
+
+            //＊＊＊＊＊＊＊＊＊＊＊＊＊決定後の処理＊＊＊＊＊＊＊＊＊＊＊＊
+            if (command.IsBbutton((int)GameManager.Instance.inputDevice))
+            {
+                //それぞれのUIの位置野処理
+                switch (change)
+                {
+                    //上の場合
+                    case 0:
+                        SceneManager.LoadScene("Title");
+                        break;
+                    //下の場合
+                    case 1:
+                        break;
+                }
+            }
         }
+
+        
     }
 }
